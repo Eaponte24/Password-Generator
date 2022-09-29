016d7a44150b966ef545b9ad4a42343910557d13
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-09-28 23:54:10 -0400

    added the first step in making the generate button functional

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+/* so far this function allows me to click on the generate button and if i dont put in a number
+i get a thats not a number response back if it is i get the is a number response */
+function generatePassword() {
+   
+    var userinput = window.prompt("How long do you want your password to be?");
+    
+    var passwordLength = parseInt(userinput)
+
+    if (isNaN(passwordLength)) {
+        window.alert("Thats not a number!")
+    } else {
+        window.alert("Thats a vaild number, good job!")
+    }
+
+    
+   
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

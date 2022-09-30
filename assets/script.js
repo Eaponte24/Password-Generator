0c2426239f69a42f183c2874227d15cc658e883a
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-09-30 17:31:14 -0400

    added notes and fixed the random number function as well as added a for loop to randomize the list items in the listbatch var

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// The function to randomize to turnt he randomized number into a whole integer
+function randomInt(min, max) {
+   if (!max) {
+    max = min
+    min = 0
+   }
+   var rand = Math.random()
+   return Math.floor(min*(1 - rand) + rand*max)
+}
+// This function randomizes the list items
+function getRandomList(list) { 
+    return list[randomInt(list.length)]
+}
 
 function generatePassword() {
    
     var userinput = window.prompt("How long do you want your password to be?");
-    
+//the parse int here is trying to covert whatever the user inputs into a number data type and the if statements are basically saying if the datatype NAN is returned then to alet thats not a number 
     var passwordLength = parseInt(userinput)
 
     if (isNaN(passwordLength)) {
@@ -17,7 +29,7 @@ function generatePassword() {
         window.alert("Password Length must be between 8 and 128 Characters")
         return
     }
-
+// These are the confirm screens for if the user wants number, symbols or lower/uppercase letter
     var userWantsNumbers = window.confirm("Would you like your password to include numbers?")
     var userWantsSymbols = window.confirm("Would you like your password to include symbols?")
     var userWantsLowercase = window.confirm("Would you like your password to include lowercase letters?")
@@ -27,14 +39,14 @@ function generatePassword() {
     var symbollist = ["!","@","#","$","%","^","&","*","(","+"]
     var lowercaselist = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","w","x","y","z"]
     var uppercaselist = []
-
+// This is where the string arrays will be sent to when the user is done choosing what they want their password to be compiled of
     var listBatch = []
-
+// This is a for loop to turn the uppercase letter array to be all uppercase letters
     for (var i = 0; i < lowercaselist.length; i++) {
         uppercaselist[i] = lowercaselist[i].toUpperCase()
     }
 
-
+// These if statments are if the user hits "okay" to the if they want number etc then those arrays will be added to the list batch var
     if (userWantsNumbers) {
         listBatch.push(numberlist)
     }
@@ -50,6 +62,16 @@ function generatePassword() {
     if (userWantsUppercase) {
         listBatch.push(uppercaselist)
     }
+//note here
+    var generatedPassword = ""
+// minus 1 cause arrays start at zero and dont want to start at 1
+    for (var i = 0; i <passwordLength; i++ ){
+     var randomList = getRandomList(listBatch)
+     var randomChar = getRandomList(randomList)
+     console.log(randomChar)
+    }
+
+
 
     
 
